d88b9cbbabb3ce0e131b652d1c895e70ebd2ca16
Siebe Albers <savdelz@gmail.com>
2023-06-17 16:33:50 +0200

    upl

---
+++ output-appData-folderSize.ps1
@@ -0,0 +1,17 @@
+
+# this script outputs the size of all folders in the AppData folder
+
+$sourcePath = "C:\Users\Siebe\AppData\Roaming"
+
+$folders = Get-ChildItem -Path $sourcePath -Directory
+
+foreach ($folder in $folders) {
+    $folderName = $folder.Name
+    $folderSize = (Get-ChildItem -Path $folder.FullName -Recurse | Measure-Object -Property Length -Sum).Sum
+
+    $formattedSize = "{0:N2} GB" -f ($folderSize / 1GB)
+
+    Write-Host "Folder Name: $folderName"
+    Write-Host "Folder Size: $formattedSize"
+    Write-Host
+}
