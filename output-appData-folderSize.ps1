db0c764759e46fa472b26f778ca38c92ccd2cba6
Siebe Albers <savdelz@gmail.com>
2023-06-18 19:34:52 +0200

    sorting output

--- output-appData-folderSize.ps1
+++ output-appData-folderSize.ps1
@@ -1,19 +1,26 @@
-
-# this script outputs the size of all folders in the AppData folder
-
 $sourcePath = "C:\Users\Siebe\AppData\Roaming"
-# $sourcePath = "C:\Users\Siebe\AppData\Roaming\Adobe"
-# $sourcePath = "C:\Users\Siebe\AppData\Roaming\Adobe\Common"
 
 $folders = Get-ChildItem -Path $sourcePath -Directory
 
-foreach ($folder in $folders) {
+$folderSizes = foreach ($folder in $folders) {
     $folderName = $folder.Name
     $folderSize = (Get-ChildItem -Path $folder.FullName -Recurse | Measure-Object -Property Length -Sum).Sum
 
     $formattedSize = "{0:N2} GB" -f ($folderSize / 1GB)
 
-    Write-Host "Folder Name: $folderName"
-    Write-Host "Folder Size: $formattedSize"
+    [PSCustomObject]@{
+        FolderName = $folderName
+        FolderSize = $folderSize
+        FormattedSize = $formattedSize
+    }
+}
+
+$sortedFolders = $folderSizes | Sort-Object -Property FolderSize
+
+foreach ($folder in $sortedFolders) {
+    Write-Host "Folder Name: $($folder.FolderName)"
+    Write-Host "Folder Size: $($folder.FormattedSize)"
     Write-Host
 }
+
+echo '`n    this was for $sourcePath `n '
