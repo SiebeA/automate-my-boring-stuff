ed7e737b871652f6ad4ea034b28758b57974aac8
Siebe Albers <savdelz@gmail.com>
2023-08-10 21:37:20 +0200

    FIX ; only title as dates are the same

--- name_videoFile_asDate.py
+++ name_videoFile_asDate.py
@@ -1,15 +1,18 @@
+# venv: audio-text
+
+
 import os
 import datetime
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
 
 # Set the path to the video file
-video_folder = "C://Users//Siebe//Desktop//upload_youtube"
+video_folder = "C://Users//Siebe//Phone_sync//podcast//The Age of Napoleon Podcast"
 
 # Loop through all the files in the folder
 for filename in os.listdir(video_folder):
-    # Check if the file is an MP4 file
-    if filename.endswith("MP4"):
+    # Check if the file is an MP3 file
+    if filename.endswith("mp3"):
         # Set the path to the video file
         video_path = os.path.join(video_folder, filename)
 
@@ -18,15 +21,25 @@ for filename in os.listdir(video_folder):
 
         # Extract the metadata from the video file
         metadata = extractMetadata(parser)
-        print('hi')
-        # Get the media creation date from the metadata
+
+        # Close the parser to release the file
+        parser.close()
 
         # Get the media creation date from the metadata
-        create_date = metadata.get("creation_date")
-        if create_date:
-            create_date_str = create_date.strftime("%Y-%m-%d %H:%M:%S")
-            create_datetime = datetime.datetime.strptime(create_date_str, "%Y-%m-%d %H:%M:%S")
-            create_date_formatted = create_datetime.strftime("%Y-%m-%d_%H-%M")
-            print(f"{filename}: {create_date_formatted}")
-        else:
-            print(f"{filename}: Media creation date not found.")
+        # create_date = metadata.get("creation_date") # outputs dates that are all the same
+        title = metadata.get("title")
+        # if create_date:
+        #     create_date_str = create_date.strftime("%Y-%m-%d %H:%M:%S")
+        #     create_datetime = datetime.datetime.strptime(create_date_str, "%Y-%m-%d %H:%M:%S")
+        #     create_date_formatted = create_datetime.strftime("%Y-%m-%d")
+
+        # Replace characters not allowed in filenames
+        valid_title = title.replace(":", "_")
+        valid_title = valid_title.replace("/", "_")
+
+        # new_filename = f"{create_date_formatted}_{valid_title}.mp3" # outputs dates that are all the same
+        new_filename = f"{valid_title}.mp3"
+        new_path = os.path.join(video_folder, new_filename)
+        os.rename(video_path, new_path)
+        # else:
+        #     print(f"{filename}: Media creation date not found.")
