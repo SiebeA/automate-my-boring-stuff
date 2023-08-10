567e41d88fcac26356029066650ad3a38599d778
Siebe Albers <savdelz@gmail.com>
2023-08-10 14:49:37 +0200

    FEAT pulling from phone
    but only works when pushing is commented out, otherwise no prompt is given in the terminal

--- phone-sync-obsidian.ps1
+++ phone-sync-obsidian.ps1
@@ -14,19 +14,20 @@ if ($adbDevicesOutput | Select-String "device") {
 } else {
     # Action when the phone is not connected to the computer
     Write-Host "The phone is not YET connected to the computer."
-    # enable wireless debugging on your phone
-    # connect your phone to your computer via USB FIRST to grant permission
-    adb tcpip 5555
+    # (enable wireless debugging on your phone)
+    
+    # adb tcpip 5555 # connect your phone to your computer via USB FIRST to grant permission
 
     adb shell ip -f inet addr show wlan0 # get your phone's IP address
 
     # connect to your phone via wireless debugging
     adb connect 192.168.2.30
+    adb start-server
     Write-Host "The phone is now connected to the computer."
 }
 
 # ==================================
-#         pulling and pushing
+#         pushing
 # ==================================
 
 # Determine the path for the PC folder and the Obsidian path
@@ -36,7 +37,12 @@ $obsidianPath = "/sdcard/documents/Siebe/"
 # List the .md files in the PC folder
 $mdFiles = Get-ChildItem -Path $pc_folder -Filter "*.md"
 # ask whether the user wants to push all files to the phone
-Write-Host "Do you want to push all .md files to the phone? (y/n)"
+# print the files in the folder
+Write-Host "The following .md files are in the folder:`n`n $pc_folder"
+foreach ($file in $mdFiles) {
+    Write-Host $file.Name
+}
+Write-Host "`n`n`nDo you want to push all .md files from the folder: $pc_folder  to the phone? (y/n)"
 $answer = Read-Host
 if ($answer -eq "y") {
     Write-Host "Pushing all .md files to the phone."
@@ -51,5 +57,38 @@ if ($answer -eq "y") {
 
 } else {
     Write-Host "Not pushing all .md files to the phone."
+    exit
+}
+
+# ==================================
+#           pulling 
+# ==================================
+
+Write-Host "Listing the files on the phone"
+# get the path to the obsidian vault on the phone
+$pc_folder = "C:\Users\Siebe\Documents\obsidian\Siebe"
+$obsidianPath = "/sdcard/documents/Siebe/"
+$mdFiles = adb shell ls "$obsidianPath*.md"
+Write-Host "The following .md files are in the folder:`n`n $obsidianPath"
+foreach ($file in $mdFiles) {
+    Write-Host $file
+}
+
+# ask whether the user wants to pull all files from the phone
+Write-Host "`n`n`nDo you want to PULL all .md files from the phone? (y/n) to the folder: $pc_folder"
+$answer = Read-Host
+if ($answer -eq "y") {
+    Write-Host "Pulling all .md files from the phone."
+    
+    # Loop through each .md file and pull it from the phone
+    foreach ($file in $mdFiles) {
+        $fileName = $file.Name
+        adb pull "$obsidianPath$fileName" "$pc_folder\$fileName"
+    }
+
+    Write-Host "All .md files have been pulled from the obsidian vault."
+
+} else {
+    Write-Host "Not pulling all .md files from the phone."
     exit
 }
