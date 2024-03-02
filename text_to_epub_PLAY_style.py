eb768a0cc2142d8fb08fff1f3d7f468ace75e48a
Siebe Albers <savdelz@gmail.com>
2024-03-02 11:42:19 -0500

    source from file

--- text_to_epub_PLAY_style.py
+++ text_to_epub_PLAY_style.py
@@ -1,25 +1,12 @@
 from bs4 import BeautifulSoup
 import re
 
-input_text = """
-Anderson: y también la referencia cualitativa que simplemente necesitábamos, creo,
-   and then this qualitative reference that we simply needed,
+# Specify the path to the input text file
+input_file_path = r'C:\Users\Siebe\Desktop\Spanish Subtitles to book\Krishnamurti\input.txt'
 
-para discernir como posibilidad.
-it seems to me, to discern as a possibility.
-
-De nuevo recuerdo una declaración que hizo y anteriormente cité
-I am reminded again of the statement that you made that I quoted earlier,
-
-'Es la responsabilidad de todos, de cada persona'.
-that it is the responsibility of each, each human person.
-
-Krishnamurti: Del ser humano, sí. 
-   Human being, yes. 
-
-Anderson: Exacto.
-   Right.
-"""
+# Open the input text file and read its contents
+with open(input_file_path, 'r', encoding='utf-8') as input_file:
+    input_text = input_file.read()
 
 # Create BeautifulSoup object
 soup = BeautifulSoup(input_text, 'html.parser')
@@ -34,6 +21,8 @@ for line in soup.get_text().split('\n'):
     line = line.strip()
     if not line:
         continue
+    # Debugging: print the content of the line
+    print("Line:", line)
     # Check if the line contains a colon
     if ':' in line:
         # If yes, update the speaker and format the accumulated dialogue
@@ -45,10 +34,15 @@ for line in soup.get_text().split('\n'):
                 formatted_dialogue += f'- {dialogue_line.strip()} <br/>'
             formatted_line = f'<tr class="first-child1">\n\t<td epub:type="z3998:persona" class="epub-type-z3998-persona1"><b>{speaker}</b></td>\n\t<td class="epub-type-z3998-verse">\n\t\t<p class="first-child3">\n\t\t\t<span class="calibre19">{formatted_dialogue}</span>\n\t\t</p>\n\t</td>\n</tr>'
             formatted_lines.append(formatted_line)
-        speaker, dialogue = re.split(r':\s+', line, maxsplit=1)
+        # Split the line into speaker and dialogue
+        try:
+            speaker, dialogue = re.split(r':\s+', line, maxsplit=1)
+        except ValueError:
+            print("Error: Could not split line into speaker and dialogue:", line)
+            continue
         dialogue_accumulator = [dialogue.strip()]
     else:
-        # If no, append the dialogue to the dialogue accumulator
+        # If no colon is found, append the dialogue to the accumulator
         if speaker:
             dialogue_accumulator.append(line)
 
